61e212926f41843d9e4458aa2b5005d2f46f08c5
krastiker <110397671+krastiker@users.noreply.github.com>
2022-10-20 12:17:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// // var counter=0;
+// // function onClickButtonAdd(el)
+// // {
+// //     counter++;
+// //     el.innerHTML =counter;
+// //     console.log(counter);
+// // }
+// // function onClickButtonMinus(el)
+// // {
+// //     counter--;
+// //     el.innerHTML =counter;
+// //     console.log(counter);
+// // }
+// // function onClickButtonReset()
+// // {
+// //     counter=0;
+// //     console.log(counter);
+// // }
+// // var spans = document.getElementsByTagName('span');
+// // var spans = document.getElementsByClassName('simple-text');
+
+// // for(var i=0; i<spans.length; i++)
+// // {
+// //     console.log(spans[i].innerHTML); 
+// // }
+
+
+
+
+// // function checkForm(el)
+// // {
+// //     var name=el.name.value;
+// //     var pass=el.pass.value;
+// //     var confPass=el.confPass.value;
+// //     var state=el.state.value; 
+// //     var fail="";
+// //     if(name=="" || pass==""||state=="")
+// //     fail="fill all champs";
+// //     else if (name.length<4)
+// //     fail="name is <4";
+// //     else if (pass!=confPass)
+// //     fail="pass is diferent";
+// //     if (fail!="")
+// //     {
+// //         document.getElementById("error").innerHTML = fail;
+// //         return false;
+// //     }
+// //     else
+// //     {
+// //         alert("all data is right");
+// //         return true;}}
+
+
+// let addMessage=document.querySelector('.message');
+// let addButton=document.querySelector('.add');
+
+// addButton.addEventListener('click', function()
+// {}
+// )
+
+ 
